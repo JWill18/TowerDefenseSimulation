49ffd234ed77553497de3dfb499d6352e79acf67
Jesse Willis <jessewillis827@gmail.com>
2018-04-22 18:31:17 -0400

    Created an AI turret that will attack any enemy that has health that is within range until the enemy is destroyed or is out of range.

---
+++ Assets/Scripts/AI/Enemy/AI_Health.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The AI health component that controls how much health there is and if the game object is already dead or not.
+/// </summary>
+public class AI_Health : MonoBehaviour {
+
+	/// <summary>
+	/// The maximum amount of health that the AI will start with
+	/// </summary>
+	public float MaxHealth;
+
+	/// <summary>
+	/// The current amount of health that the AI currently has
+	/// </summary>
+	private float CurrentHealth;
+
+	/// <summary>
+	/// Is the AI dead or not?
+	/// </summary>
+	public bool IsDead;
+
+	// Use this for initialization
+	void Start () {
+		CurrentHealth = MaxHealth;
+		IsDead = false;
+	}
+
+	/// <summary>
+	/// Used to deal damage to self
+	/// </summary>
+	/// <param name="amount">How much damage is being taken.</param>
+	public void TakeDamage(float amount)
+	{
+		// Decrease health
+		CurrentHealth -= amount;
+
+		// If the AI is dead, then play death animation
+		if(CurrentHealth <= 0)
+		{
+			IsDead = true;
+			Death();
+		}
+	}
+
+	/// <summary>
+	/// Used to do any necessary work to destroy object
+	/// </summary>
+	public void Death()
+	{
+		// Destroy object
+		Destroy(gameObject);
+	}
+}
