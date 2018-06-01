526e7a67bea131988144d450b81a5f3bca266cb8
Jesse Willis <jessewillis827@gmail.com>
2018-06-01 00:01:23 -0400

    Created the camera and updated the project files.

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float MovementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetAxis("Vertical") != 0)
+		{
+			transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetAxis("Horizontal") != 0)
+		{
+			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime);
+		}
+	}
+}
