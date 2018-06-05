2964888b54d02106e942e4062141f53d6330f606
Jesse Willis <jessewillis827@gmail.com>
2018-06-05 00:20:45 -0400

    Add CI file

---
+++ Assets/Scripts/Editor/BuildManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using System;
+
+public class BuildManager {
+
+	public static string[] args = Environment.GetCommandLineArgs();
+
+	private static string GetArgument(string name)
+	{
+		string argumentValue = "";
+
+		for(int i = 0; i < args.Length; i++)
+		{
+			if(args[i].Contains(name))
+			{
+				argumentValue = args[i + 1];
+			}
+		}
+
+		return argumentValue;
+	}
+
+	private static BuildTarget GetBuildTarget()
+	{
+		var customTarget = GetArgument("customBuildTarget");
+
+		return customTarget == "StandaloneWindows64" ? BuildTarget.StandaloneWindows64 : BuildTarget.NoTarget;
+	}
+
+	public static void PerformBuild()
+	{
+		string[] scenes = { "Assets/Scenes/Scn_Level_01.unity" };
+		var buildOptions = new BuildPlayerOptions
+		{
+			scenes = scenes,
+			target = BuildTarget.StandaloneWindows64,
+			targetGroup = BuildTargetGroup.Standalone,
+			locationPathName = GetArgument("customBuildPath")
+		};
+
+		var results = BuildPipeline.BuildPlayer(buildOptions);
+
+		if(results.summary.totalErrors == 0)
+		{
+			EditorApplication.Exit(0);
+		}
+		else
+		{
+			EditorApplication.Exit(1);
+		}
+	}
+}
